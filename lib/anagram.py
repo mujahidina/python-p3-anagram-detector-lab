70f0850be78c717add38fc97f5b53da9249f1571
mujahidina <mujahidabdi5070@gmail.com>
2024-03-03 15:29:45 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    
+    def match(self, anagrams):
+        sorted_word = sorted(self.word.lower())
+        matches = []
+        for anagram in anagrams:
+            if sorted(sorted_word) == sorted(anagram.lower()):
+                matches.append(anagram)
+        return matches
+            
+mujahid = Anagram('listen')
+matches = mujahid.match(['enlists', 'google', 'inlets', 'banana', 'enlists', 'nistel'])
+print(matches)
+
+
